ff8770b06eda451b072f2088554012576770c04e
han4solecode <farhan@solecode.id>
2024-09-18 21:45:32 +0700

    feat: create Company Request Repo

---
+++ AsriATS/Infrastructure/AsriATS.Persistance/Repositories/CompanyRequestRepository.cs
@@ -0,0 +1,48 @@
+using AsriATS.Application.Persistance;
+using AsriATS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsriATS.Persistance.Repositories
+{
+    public class CompanyRequestRepository : ICompanyRequestRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CompanyRequestRepository(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public async Task CreateAsync(CompanyRequest entity)
+        {
+            await _context.CompanyRequests.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(CompanyRequest entity)
+        {
+            _context.CompanyRequests.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<CompanyRequest>> GetAllAsync()
+        {
+            var companyRequests = await _context.CompanyRequests.Include(cr => cr.ProcessIdNavigation).ToListAsync();
+
+            return companyRequests;
+        }
+
+        public async Task<CompanyRequest?> GetByIdAsync(int id)
+        {
+            var companyRequest = await _context.CompanyRequests.Include(cr => cr.ProcessIdNavigation).FirstOrDefaultAsync();
+
+            return companyRequest;
+        }
+
+        public async Task UpdateAsync(CompanyRequest entity)
+        {
+            _context.CompanyRequests.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
