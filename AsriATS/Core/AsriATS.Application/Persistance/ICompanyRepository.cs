0e2e624066cbe85c25ac1d6554fc797e3e622756
han4solecode <farhan@solecode.id>
2024-09-18 10:08:37 +0700

    feat: create company repo interface

---
+++ AsriATS/Core/AsriATS.Application/Persistance/ICompanyRepository.cs
@@ -0,0 +1,10 @@
+using AsriATS.Application.Persistance.Common;
+using AsriATS.Domain.Entities;
+
+namespace AsriATS.Application.Persistance
+{
+    public interface ICompanyRepository : IBaseRepository<Company>
+    {
+        
+    }
+}
