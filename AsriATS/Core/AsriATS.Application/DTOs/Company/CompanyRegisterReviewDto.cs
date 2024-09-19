b445afb079c367c7ab1b1d11890abc44af54f24e
han4solecode <farhan@solecode.id>
2024-09-19 12:21:59 +0700

    create DTO for company registration review

---
+++ AsriATS/Core/AsriATS.Application/DTOs/Company/CompanyRegisterReviewDto.cs
@@ -0,0 +1,9 @@
+namespace AsriATS.Application.DTOs.Company
+{
+    public class CompanyRegisterReviewDto
+    {
+        public int CompanyRequestId { get; set; }
+
+        public string Action { get; set; } = null!;
+    }
+}
