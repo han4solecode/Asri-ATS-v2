b28c5fcb4bbfeaa298fbbcf47292a65d7820ac35
han4solecode <farhan@solecode.id>
2024-09-19 13:57:23 +0700

    create endpoint for company registration and review

--- AsriATS/Presentation/AsriATS.WebAPI/Controllers/CompanyController.cs
+++ AsriATS/Presentation/AsriATS.WebAPI/Controllers/CompanyController.cs
@@ -1,4 +1,5 @@
 using AsriATS.Application.Contracts;
+using AsriATS.Application.DTOs.Company;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AsriATS.WebAPI.Controllers
@@ -14,6 +15,40 @@ namespace AsriATS.WebAPI.Controllers
             _companyService = companyService;
         }
 
-        
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterCompany([FromBody] CompanyRegisterRequestDto companyRegisterRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var res = await _companyService.CompanyRegisterRequestAsync(companyRegisterRequest);
+
+            if (res.Status == "Error")
+            {
+                return BadRequest(res.Message);
+            }
+
+            return Ok(res);
+        }
+
+        [HttpPost("review-request")]
+        public async Task<IActionResult> ReviewCompanyRegisterRequest([FromBody] CompanyRegisterReviewDto companyRegisterReview)
+        {
+             if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var res = await _companyService.ReviewCompanyRegisterRequest(companyRegisterReview);
+
+            if (res.Status == "Error")
+            {
+                return BadRequest(res.Message);
+            }
+
+            return Ok(res);
+        }
     }
 }
