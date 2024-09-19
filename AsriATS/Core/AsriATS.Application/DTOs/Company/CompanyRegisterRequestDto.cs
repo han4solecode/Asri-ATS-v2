777165d1841365310c5fcf22e945b9668944cce2
han4solecode <farhan@solecode.id>
2024-09-19 12:03:23 +0700

    add props

--- AsriATS/Core/AsriATS.Application/DTOs/Company/CompanyRegisterRequestDto.cs
+++ AsriATS/Core/AsriATS.Application/DTOs/Company/CompanyRegisterRequestDto.cs
@@ -4,8 +4,20 @@ namespace AsriATS.Application.DTOs.Company
     {
         public string Email { get; set; } = null!;
 
-        public string Name { get; set; } = null!;
+        public string CompanyName { get; set; } = null!;
 
-        public string Address { get; set; } = null!;
+        public string CompanyAddress { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string UserAddress { get; set; } = null!;
+
+        public DateOnly Dob { get; set; }
+
+        public string Sex { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
     }
 }
