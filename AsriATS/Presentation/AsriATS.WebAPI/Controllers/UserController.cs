8a699e168cedf7903b8ffbd31791ab6f4164cd4b
risyadkamarullahsolecode1 <risyad.kamarullah@solecode.id>
2024-09-19 13:00:56 +0700

    add user interface, service, controllers

---
+++ AsriATS/Presentation/AsriATS.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using AsriATS.Application.Contracts;
+using AsriATS.Application.DTOs.ChangePassword;
+using AsriATS.Application.DTOs.Update;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.Ocsp;
+
+namespace AsriATS.WebAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPut("update-user")]
+        public async Task<IActionResult> UpdateUser([FromBody] UpdateRequestDto update)
+        {
+            var res = await _userService.UpdateUserAsync(update);
+            return Ok(res);
+        }
+
+        [HttpGet("user-info")]
+        public async Task<IActionResult> GetUserInfo()
+        {
+            try
+            {
+                var user = await _userService.GetUserInfo();
+                return Ok(user);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Unauthorized("User not logged in.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("ChangePassword")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequestDto change)
+        {
+            var result = await _userService.ChangePasswordAsync(change);
+            if (result.Status == "Error")
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok(result.Message);
+        }
+    }
+}
