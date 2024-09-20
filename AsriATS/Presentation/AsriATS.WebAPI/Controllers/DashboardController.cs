fd693b215f42a08ce0d90106c0b41dc9c8d8e444
han4solecode <farhan@solecode.id>
2024-09-20 10:44:20 +0700

    add endpoint for retrieving all users to be used by admin

--- AsriATS/Presentation/AsriATS.WebAPI/Controllers/DashboardController.cs
+++ AsriATS/Presentation/AsriATS.WebAPI/Controllers/DashboardController.cs
@@ -9,10 +9,12 @@ namespace AsriATS.WebAPI.Controllers
     public class DashboardController : ControllerBase
     {
         private readonly ICompanyService _companyService;
+        private readonly IUserService _userService;
 
-        public DashboardController(ICompanyService companyService)
+        public DashboardController(ICompanyService companyService, IUserService userService)
         {
             _companyService = companyService;
+            _userService = userService;
         }
         
         [Authorize(Roles = "Administrator")]
@@ -22,6 +24,15 @@ namespace AsriATS.WebAPI.Controllers
             var cr = await _companyService.GetAllCompanyRegisterRequest();
 
             return Ok(cr);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("users")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userService.GetAllUserInfoAsync();
+
+            return Ok(users);
         }
     }
 }
