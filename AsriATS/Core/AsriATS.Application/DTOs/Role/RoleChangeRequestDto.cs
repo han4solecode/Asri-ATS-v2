98ff6313c621f3c97ef28b7073e1e5982b66b0c9
han4solecode <farhan@solecode.id>
2024-09-22 22:43:08 +0700

    create role change request DTO

---
+++ AsriATS/Core/AsriATS.Application/DTOs/Role/RoleChangeRequestDto.cs
@@ -0,0 +1,9 @@
+namespace AsriATS.Application.DTOs.Role
+{
+    public class RoleChangeRequestDto
+    {
+        public int RoleChangeRequestId { get; set; }
+
+        public string Action { get; set; } = null!;
+    }
+}
