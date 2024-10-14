aff4928f46261ec4b7c7862b1d6121273bf4d40a
han4solecode <farhan@solecode.id>
2024-10-14 22:51:10 +0700

    add endpoint documentation for auth controller

--- AsriATS/Presentation/AsriATS.WebAPI/Controllers/AuthController.cs
+++ AsriATS/Presentation/AsriATS.WebAPI/Controllers/AuthController.cs
@@ -20,8 +20,28 @@ namespace AsriATS.WebAPI.Controllers
             _authService = authService;
         }
 
-        //Create Applicant
-        //[Authorize(Roles = "Applicant, Administrator")]
+        /// <summary>
+        /// Create a new applicant user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST /api/auth/applicant
+        ///     {
+        ///         "username": "applicant2",
+        ///         "firstname": "Bang",
+        ///         "lastname": "Rondo",
+        ///         "email": "bangrondo@gmail.com",
+        ///         "password": "Ab123456",
+        ///         "address": "Jl. Tembus, Jakarta",
+        ///         "dob": "2002-02-22",
+        ///         "sex": "Male",
+        ///         "phoneNumber": "0811437661"
+        ///     }
+        /// 
+        /// </remarks>
+        /// <param name="register"></param>
+        /// <returns></returns>
         [HttpPost("applicant")]
         public async Task<IActionResult> CreateApplicant([FromBody] RegisterRequestDto register)
         {
@@ -37,8 +57,30 @@ namespace AsriATS.WebAPI.Controllers
             return Ok(res);
         }
 
-        //Create User and assign to a spesific role
-        //[Authorize(Roles = "Administrator")]
+        /// <summary>
+        /// Create a new user with a specific role
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST /api/auth/create-user?rolename={rolename}
+        ///     {
+        ///         "username": "admin",
+        ///         "firstname": "Admin",
+        ///         "lastname": "Cool",
+        ///         "email": "admin@gmail.com",
+        ///         "password": "Ab123456",
+        ///         "address": "Jl Ngasal, Bandung",
+        ///         "dob": "1998-02-11",
+        ///         "sex": "Female",
+        ///         "phoneNumber": "0814018739"
+        ///     }
+        /// 
+        /// </remarks>
+        /// <param name="register"></param>
+        /// <param name="rolename"></param>
+        /// <returns></returns>
+        [Authorize(Roles = "Administrator")]
         [HttpPost("create-user")]
         public async Task<IActionResult> CreateUser([FromBody] RegisterUserRequestDto register, string rolename)
         {
@@ -54,8 +96,7 @@ namespace AsriATS.WebAPI.Controllers
             return Ok(res);
         }
 
-        //update or manage own applicant data
-        //[Authorize(Roles = "Applicant, Administrator")]
+        [Authorize(Roles = "Applicant, Administrator")]
         [HttpPut("update")]
         public async Task<IActionResult> ManageApplicant([FromBody] UpdateRequestDto update)
         {
@@ -71,6 +112,21 @@ namespace AsriATS.WebAPI.Controllers
             return Ok(res);
         }
 
+        /// <summary>
+        /// Login a user account
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST /api/auth/login
+        ///     {
+        ///         "username": "applicant1",
+        ///         "password": "Ab123456"
+        ///     }
+        /// 
+        /// </remarks>
+        /// <param name="login"></param>
+        /// <returns></returns>
         [HttpPost("login")]
         public async Task<IActionResult> LoginAsync([FromBody] LoginRequestDto login)
         {
@@ -88,7 +144,21 @@ namespace AsriATS.WebAPI.Controllers
             return Ok(res);
         }
 
-        //change password
+        /// <summary>
+        /// Change user password
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST /api/auth/changepassword
+        ///     {
+        ///         "newPassword": "Ab123456"
+        ///     }
+        ///     
+        /// </remarks>
+        /// <param name="change"></param>
+        /// <returns></returns>
+        [Authorize]
         [HttpPost("ChangePassword")]
         public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequestDto change)
         {
